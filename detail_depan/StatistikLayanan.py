981db6c3630ef1a0fef6d87a8d02fda3bfcdc407
gayuhsuwiatmaja <60841397+gayuhsuwiatmaja@users.noreply.github.com>
2020-02-14 08:00:49 +0700

    Update StatistikLayanan.py

--- detail_depan/StatistikLayanan.py
+++ detail_depan/StatistikLayanan.py
@@ -84,7 +84,7 @@ def StatLayanan_main(bulan,nama_mpp):
     # Change the bar mode
    
     fig.update_layout(barmode='group')
-    fig.update_layout(title_text='Statistik Pengguna Layanan ' + nama_mpp + ' di ' + bulan)
+    fig.update_layout(title_text='Statistik Pengguna Layanan ' + nama_mpp + ' di ' + bulan, autosize = True, width=1500, height=800)
     
     st.plotly_chart(fig)
     
